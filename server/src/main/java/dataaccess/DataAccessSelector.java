48cbcaf12e2ac2eb9a0172d3db2b2efbd0094352
Joseph-Tuft <jrtuft30@gmail.com>
2026-05-25 19:23:37 -0600

    impl(p4) it initializes the database!

--- server/src/main/java/dataaccess/DataAccessSelector.java
+++ server/src/main/java/dataaccess/DataAccessSelector.java
@@ -1,24 +1,14 @@
 package dataaccess;
 
+import dataaccess.exceptions.ResponseException;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class DataAccessSelector {
-    private static final boolean USE_SQL = false;
+    private static final boolean USE_SQL = true;
 
-    public static UserDAO getUserDAO() {
-        return USE_SQL ? new SQLUserDAO() : new MemoryUserDAO();
-    }
-
-    public static AuthDAO getAuthDAO() {
-        return USE_SQL ? new SQLAuthDAO() : new MemoryAuthDAO();
-    }
-
-    public static GameDAO getGameDAO() {
-        return USE_SQL ? new SQLGameDAO() : new MemoryGameDAO();
-    }
-
-    private final String[] createStatements = {
+    private static final String[] createStatements = {
             """
             CREATE TABLE IF NOT EXISTS  authList (
               `id` int NOT NULL AUTO_INCREMENT,
@@ -30,15 +20,16 @@ public class DataAccessSelector {
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
             """,
             """
-            CREATE TABLE IF NOT EXISTS  authList (
+            CREATE TABLE IF NOT EXISTS  userList (
               `id` int NOT NULL AUTO_INCREMENT,
               `username` varchar(256) NOT NULL,
               `password` varchar(256) NOT NULL,
               `email` varchar(256) NOT NULL,
               PRIMARY KEY (`id`),
               INDEX(username),
-              INDEX(password),
-              INDEX(email)
+              UNIQUE KEY (`username`),
+              INDEX(email),
+              UNIQUE KEY (`email`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
             """,
             """
@@ -56,7 +47,25 @@ public class DataAccessSelector {
             """
     };
 
-    private void configureDatabase() throws ResponseException {
+    static {
+        if (USE_SQL){
+            configureDatabase();
+        }
+    }
+
+    public static UserDAO getUserDAO() {
+        return USE_SQL ? new SQLUserDAO() : new MemoryUserDAO();
+    }
+
+    public static AuthDAO getAuthDAO() {
+        return USE_SQL ? new SQLAuthDAO() : new MemoryAuthDAO();
+    }
+
+    public static GameDAO getGameDAO() {
+        return USE_SQL ? new SQLGameDAO() : new MemoryGameDAO();
+    }
+
+    private static void configureDatabase() throws ResponseException {
         DatabaseManager.createDatabase();
         try (Connection conn = DatabaseManager.getConnection()) {
             for (String statement : createStatements) {
@@ -65,7 +74,7 @@ public class DataAccessSelector {
                 }
             }
         } catch (SQLException ex) {
-            throw new ResponseException(ResponseException.Code.ServerError, String.format("Unable to configure database: %s", ex.getMessage()));
+            throw new ResponseException(String.format("Unable to configure database: %s", ex.getMessage()));
         }
     }
 
