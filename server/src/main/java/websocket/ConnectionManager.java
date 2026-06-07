e6064f1419dde023b7c572128a5a5a58b1c98b3a
Joseph-Tuft <jrtuft30@gmail.com>
2026-06-07 10:43:24 -0600

    impl(p6) Finished connection manager

---
+++ server/src/main/java/websocket/ConnectionManager.java
@@ -0,0 +1,38 @@
+package websocket;
+
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConnectionManager {
+    public final ConcurrentHashMap<Integer, List<Session>> connections = new ConcurrentHashMap<>();
+
+    public void add(Integer gameId, Session session) {
+        if (connections.containsKey(gameId)){
+            connections.get(gameId).add(session);
+        }
+        else {
+            connections.put(gameId, new ArrayList<>());
+            connections.get(gameId).add(session);
+        }
+    }
+
+    public void remove(Integer gameId, Session session) {
+        connections.get(gameId).remove(session);
+    }
+
+    public void broadcast(Integer gameId, Session excludeSession, ServerMessage notification) throws IOException {
+        String msg = notification.toString();
+        for (Session c : connections.get(gameId)) {
+            if (c.isOpen()) {
+                if (!c.equals(excludeSession)) {
+                    c.getRemote().sendString(msg);
+                }
+            }
+        }
+    }
+}
